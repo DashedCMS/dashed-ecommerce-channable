c608c467b5e4f7420c9d4f965ace00359819a2a1
Robinvm <Robinvm@users.noreply.github.com>
2022-03-02 08:44:02 +0000

    Fix styling

--- src/Controllers/ChannableController.php
+++ src/Controllers/ChannableController.php
@@ -4,10 +4,10 @@ namespace Qubiqx\QcommerceEcommerceChannable\Controllers;
 
 use Illuminate\Support\Facades\App;
 use Qubiqx\QcommerceCore\Classes\Locales;
-use Qubiqx\QcommerceCore\Models\Customsetting;
-use Qubiqx\QcommerceEcommerceCore\Models\Product;
 use Qubiqx\QcommerceCore\Controllers\Frontend\FrontendController;
+use Qubiqx\QcommerceCore\Models\Customsetting;
 use Qubiqx\QcommerceEcommerceChannable\Resources\ChannableProductResource;
+use Qubiqx\QcommerceEcommerceCore\Models\Product;
 
 class ChannableController extends FrontendController
 {
