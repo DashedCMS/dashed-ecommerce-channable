d4accbab23392a5c160d79a42e8b26485cdbfb1c
Robin van Maasakker <robin@qubiqx.com>
2025-05-23 23:05:04 +0200

    Update

--- routes/channableRoutes.php
+++ routes/channableRoutes.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Dashed\DashedEcommerceChannable\Controllers\ChannableController;
 
-if (! app()->runningInConsole()) {
-    Route::get('/channable-feed/{locale}', [ChannableController::class, 'index']);
-}
+Route::get('/channable-feed/{locale}', [ChannableController::class, 'index']);
